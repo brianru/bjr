efb982578b657f9d27b69692d6fee175aa59a13e
brianru <brianrubinton@gmail.com>
2014-07-26 17:36:35 -0400

    placeholder site

--- src/brian/routes/home.clj
+++ src/brian/routes/home.clj
@@ -3,16 +3,28 @@
             [brian.views.layout :as layout]
             [hiccup.core :refer :all]))
 
+
+(defn icon [url img]
+  (let [x [:a {:href url}
+           [:img.icon {:src img}]]]
+    x))
+
+(def twitter-icon  (icon "//twitter.com/brianru" "/img/twitter.png"))
+(def github-icon   (icon "//github.com/brianru" "/img/github.png"))
+(def linkedin-icon (icon "https://www.linkedin.com/in/brianjrubinton"
+                         "/img/linkedin.png"))
+
 (defn home []
-  (layout/common [:div.top-banner
-                  [:div "Hello World!"]]
-                 [:h2 "My name is Brian James Rubinton."]
-                 [:br]
-                 [:h2 "This is my website."]
-                 [:div [:span "links to stuff"]]
-                 [:div [:span "github stats go here"]]
-                 [:div [:span "other stats"]]
-                 ))
+  (layout/common
+    [:div.top-banner
+     [:div [:ul
+            [:li [:span.my-name "Brian James Rubinton"]]
+            [:li github-icon twitter-icon linkedin-icon]]]]
+    ;[:h2 "This is my website."]
+    ;[:div [:span "links to stuff"]]
+    ;[:div [:span "github stats go here"]]
+    ;[:div [:span "other stats"]]
+    ))
 
 ; top: big, full screen-height image with intro in the middle (height: 100vh)
 ;      and a button to contact me. maybe it opens a form right there,
