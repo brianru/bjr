5ba2fd6e5da237ed6841f0e3c039cb1243c7d5ec
brianru <brianrubinton@gmail.com>
2014-07-28 12:16:03 -0400

    styling, fixes #1 and #2

--- src/brian/models/schema.clj
+++ src/brian/models/schema.clj
@@ -1,3 +1,12 @@
 (ns brian.models.schema
   (:require [brian.models.db :refer :all]
             [clojure.java.jdbc :as sql]))
+
+(comment
+(defn create-books-table []
+  (sql/with-connection db
+    (sql/create-table
+      :books
+      []
+      [])))
+  )
