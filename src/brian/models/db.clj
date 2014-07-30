65884efc5528b5d107d196bcd0a6988d5a221bc4
brianru <brianrubinton@gmail.com>
2014-07-29 21:42:27 -0400

    learning datalog

--- src/brian/models/db.clj
+++ src/brian/models/db.clj
@@ -1,19 +1,52 @@
 (ns brian.models.db
-  (:require [clojure.java.jdbc :as sql]
-            [clojure-csv.core :as csv]
-            [environ.core :refer [env]]
-            [korma.db :refer [defdb transaction]]
-            [korma.core :refer :all]))
+  (:require [clojure-csv.core :as csv])
+  (:use [fogus.datalog.bacwn :only (build-work-plan run-work-plan)]
+        [fogus.datalog.bacwn.macros :only (<- ?- make-database)]
+        [fogus.datalog.bacwn.impl.rules :only (rules-set)]
+        [fogus.datalog.bacwn.impl.database :only (add-tuples)]))
 
-(def db {:subprotocol "postgresql"
-         :subname (env :db-url)
-         :user (env :db-user)
-         :password (env :db-pass)})
+
+(def db-base
+  (make-database
+    (relation :book [:isbn :author :title :pagecount])
+    (index :book :isbn)
+
+    (relation :start [:isbn :when])
+    (index :start :isbn)
+
+    (relation :finish [:isbn :when])
+    (index :finish :isbn)))
 
 ; (parse-tabular-data (env :raw-data) \tab)
-(defn parse-tabular-data [path delim]
+(defn get-tabular-data [path delim]
   (let [tsv  (csv/parse-csv (slurp path) :delimiter delim)
         hdrs (first tsv)
         rows (rest tsv)]
     (mapv #(zipmap hdrs %) rows)))
 
+; TODO how do I only iterate through the entries once?
+; TODO how do I use a map instead of the two vectors?
+; FIXME this doesn't work.
+(defn parse-entries [relation d old-ks new-ks]
+  (let [filtered (mapv #(select-keys % old-ks) d)]
+    (mapv #(cons relation (zipmap new-ks (vals %))) filtered)))
+
+(defn parse-books [d]
+  (parse-entries :book d
+                 ["Title" "Author" "ISBN"]
+                 [:title :author :isbn]))
+(defn parse-starts [d]
+  (parse-entries :start d
+                 ["ISBN" "DateAdded"]
+                 [:isbn :when]))
+(defn parse-finishes [d]
+  (parse-entries :finish d
+                 ["ISBN" "DateRead"]
+                 [:isbn :when]))
+
+(def db
+  (let [d (get-tabular-data "resources/shelfari_data.tsv" \tab)]
+    (apply (partial add-tuples db-base)
+           (concat (parse-books d)
+                   (parse-starts d)
+                   (parse-finishes d)))))
--- src/brian/models/schema.clj
+++ src/brian/models/schema.clj
@@ -1,12 +1,2 @@
 (ns brian.models.schema
-  (:require [brian.models.db :refer :all]
-            [clojure.java.jdbc :as sql]))
-
-(comment
-(defn create-books-table []
-  (sql/with-connection db
-    (sql/create-table
-      :books
-      []
-      [])))
-  )
+  (:require [brian.models.db :refer :all]))
